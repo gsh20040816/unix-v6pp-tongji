df47c1f96a7648a0b0a7508f718984b82eac383b
gsh <gsh20040816@gmail.com>
2026-04-04 23:27:54 +0800

    build: remove make system support

--- src/boot/sector2.s
+++ src/boot/sector2.s
@@ -10,7 +10,7 @@ greatstart:
 	mov eax,2
 	mov eax,3
 
-;Makefile中g++选项 -nostartfiles禁止了g++去链接startup code,
+;构建选项中的 -nostartfiles 禁止了 g++ 去链接 startup code,
 ;startup code即是在进入我们用C++编写的main0()函数之前，以及main0()
 ;退出时执行的代码，其执行的工作是初始化(/销毁)global/static对象。
 	call _main		;call our own startup code
